03a68caada36d25a6be862c1af3f3ad8398a03b6
YunaOffc <159520550+YunaOffc@users.noreply.github.com>
2024-08-26 12:01:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 // Koneksi ke MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
