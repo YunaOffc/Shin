f5aaa0cdaacd78b2bc255520e0f52badf5b88db5
YunaOffc <159520550+YunaOffc@users.noreply.github.com>
2024-11-21 08:40:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const admin = require("firebase-admin");
+const path = require("path");
 require("dotenv").config();
 
 // Firebase Admin Setup
